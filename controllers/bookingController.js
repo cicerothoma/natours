b328e184e2c18aa55b33d881d2c1f31e7de57d8c
Collins Thomas <thomascollins582@gmail.com>
2020-09-20 03:17:39 +0100

    integrating stripe into the back-end

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,3 +1,4 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('./../models/tourModel');
 const factory = require('./../controllers/handlerFactory');
 const catchAsync = require('./../utils/catchAsync');
@@ -5,12 +6,35 @@ const AppError = require('./../utils/appError');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) get the currently booked tour
-  const tour = await Tour.findById(req.param.tourID);
+  const tour = await Tour.findById(req.params.tourID);
 
   if (!tour) {
     return next(new AppError(`Tour Not Found`, 404));
   }
   // 2) Create the checkout session
+  const session = await stripe.checkout.sessions.create({
+    // Information about session
+    payment_method_types: ['card'],
+    success_url: `${req.protocol}://${req.get('host')}/`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    customer_email: `${req.user.email}`,
+    client_reference_id: req.params.tourID,
+    // Information about product
+    line_items: [
+      {
+        name: `${tour.name} Tour`,
+        description: `${tour.summary}`,
+        images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+        amount: tour.price * 100,
+        currency: 'usd',
+        quantity: 1,
+      },
+    ],
+  });
 
   // 3) Create session as response
+  res.status(200).json({
+    status: 'success',
+    session,
+  });
 });
