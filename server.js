76cec9e6ef6c3297879fc1db2468c9d0e7f07c66
Collins Thomas <thomascollins582@gmail.com>
2020-09-23 00:50:23 +0100

    Replaced Local Database With MongoDB Atlas

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ process.on('uncaughtException', (err) => {
 
 const app = require('./app');
 
-const DB = process.env.DATABASE_LOCAL;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
 mongoose
   .connect(
